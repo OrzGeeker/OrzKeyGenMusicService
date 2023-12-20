82a88a23d43e09598828bb7af1059066265461ee
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-20 22:48:13 +0800

    generate keygen music file json

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # OrzKeyGenMusicService
 
 Backend Service About [KeyGen Music](http://keygenmusic.org/)
+
+## API
+
+path: `/musics/list`
+
+Method: `GET`
+
+
---
+++ Sources/App/Controllers/KeyGenMusicController.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by joker on 12/20/23.
+//
+
+import Foundation
+import Vapor
+
+struct KeyGenMusicController: RouteCollection {
+    
+    func boot(routes: Vapor.RoutesBuilder) throws {
+        
+        let musics = routes.grouped("musics")
+        
+        musics.get("list", use: musicList)
+    }
+
+    func musicList(req: Request) throws -> String {
+
+        let workingDirectoryURL = URL(fileURLWithPath: req.application.directory.workingDirectory)
+        let task = Process()
+        task.currentDirectoryURL = workingDirectoryURL
+        task.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+        task.arguments = [
+            "bash",
+            "-c",
+            "tree -J . | jq"
+        ]
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        try task.run()
+        
+        guard let outputData = try outputPipe.fileHandleForReading.readToEnd(),
+              let output = String(data: outputData, encoding: .utf8)
+        else {
+            return ""
+        }
+        return output
+        
+    }
+}
