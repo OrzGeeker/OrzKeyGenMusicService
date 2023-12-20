b967bc4c10b9fcd55fa3aa8c79e885bcd164606a
wangzhizhou <wangzhizhou@bytedance.com>
2023-12-20 22:08:56 +0800

    Public File server

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.3"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
     ],
     targets: [
         .target(
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,22 +5,26 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
+    
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.databases.use(
+        .postgres(
+            configuration: .init(
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+                tls: .disable)
+        ),
+        as: .psql
+    )
 
     app.migrations.add(CreateTodo())
 
     app.views.use(.leaf)
-
-    
 
     // register routes
     try routes(app)
